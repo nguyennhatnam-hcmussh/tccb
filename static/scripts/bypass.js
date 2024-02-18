fa19d50bc54c3ea8da60f4b02702f13091cae640
Nguyen Nhat Nam <nguyennhatnam@hcmussh.edu.vn>
2024-02-18 12:28:23 +0700

    second commit

--- static/scripts/bypass.js
+++ static/scripts/bypass.js
@@ -1,4 +1,4 @@
-htmx.defineExtension('bypass_reminder', {
+htmx.defineExtension('bypass', {
     onEvent: function (name, evt) {
         if (name === "htmx:configRequest") {
             evt.detail.headers['bypass-tunnel-reminder'] = 'true';
